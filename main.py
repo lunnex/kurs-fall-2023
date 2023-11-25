049c9861312c8f3d4f138329591eee5dc3a1a206
DESKTOP-I7KGGJB\ilyak <ilyakorotkih62@gmail.com>
2023-11-26 00:49:39 +0300

    commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import ctypes
+import os
+import cv2
+import numpy as np
+
+os.add_dll_directory(r"C:\Users\ilyak\source\repos\kurs-fall-2023\opencv\build\x64\vc16\bin")
+hllDll = ctypes.CDLL(r"C:\Users\ilyak\source\repos\kurs-fall-2023\x64\Release\omp-dll.dll")
+
+picPath = r"C:\Users\ilyak\source\repos\kurs-fall-2023\pic.PNG".encode('utf-8')
+
+hllDll.Cols.restype = ctypes.c_int
+hllDll.Cols.argtype = ctypes.POINTER(ctypes.c_char)
+cols = hllDll.Cols(picPath)
+print(cols)
+
+hllDll.Cols.restype = ctypes.c_int
+hllDll.Cols.argtype = ctypes.POINTER(ctypes.c_char)
+rows = hllDll.Cols(picPath)
+print(rows)
+
+
+array = (ctypes.c_int * cols * rows)()
+hllDll.Dilatation.restype = ctypes.POINTER(ctypes.c_int)
+hllDll.Dilatation.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char)]
+
+ret = hllDll.Dilatation(6, picPath)
+
+shape = (rows, cols, 4)
+x = np.zeros(shape)
+for i in range(0, rows):
+    #y = np.array([], dtype=np.uint32)
+    for j in range(0, cols):
+        if(ret[i*cols + j] == 1):
+            x[i][j] = [255, 255, 255, 255]
+        else:
+            x[i][j] = [0, 0, 0, 255]
+
+a = cv2.imread(r"C:\Users\ilyak\source\repos\kurs-fall-2023\pic.PNG")
+#print(a)
+
+
+print (x.astype((np.uint8)))
+
+vis2 = cv2.cvtColor(x.astype((np.uint8)), cv2.COLOR_RGB2BGR)
+cv2.imshow("pic", vis2)
+cv2.waitKey(0)
+
+# closing all open windows
+cv2.destroyAllWindows()
---
+++ omp-dll/pch.h
@@ -0,0 +1,154 @@
+﻿// pch.h: это предварительно скомпилированный заголовочный файл.
+// Перечисленные ниже файлы компилируются только один раз, что ускоряет последующие сборки.
+// Это также влияет на работу IntelliSense, включая многие функции просмотра и завершения кода.
+// Однако изменение любого из приведенных здесь файлов между операциями сборки приведет к повторной компиляции всех(!) этих файлов.
+// Не добавляйте сюда файлы, которые планируете часто изменять, так как в этом случае выигрыша в производительности не будет.
+
+#ifndef PCH_H
+#define PCH_H
+
+// Добавьте сюда заголовочные файлы для предварительной компиляции
+#include "framework.h"
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+struct Picture {
+	std::vector<std::vector<double>> ptrDst;
+	std::vector<std::vector<double>> ptrDstCopy;
+	int cols;
+	int rows;
+};
+
+Picture GetPicture(std::string path)
+{
+	cv::Mat dst;
+	cv::Mat img_grayscale = cv::imread(path, 0);
+	cv::Mat binaryMat(img_grayscale.size(), img_grayscale.type());
+	cv::threshold(img_grayscale, binaryMat, 100, 255, cv::THRESH_BINARY);
+
+
+	binaryMat.convertTo(dst, CV_64F, 1, 0);
+
+	Picture picture;
+
+
+	picture.ptrDst = std::vector<std::vector<double>>(dst.rows);
+	picture.ptrDstCopy = std::vector<std::vector<double>>(dst.rows);
+
+	for (int i = 0; i < dst.rows; i++)
+	{
+		dst.row(i).reshape(1, 1).copyTo(picture.ptrDst[i]);
+		dst.row(i).reshape(1, 1).copyTo(picture.ptrDstCopy[i]);
+	}
+
+	for (int i = 0; i < dst.rows; i++) {
+		for (int j = 0; j < dst.cols; j++) {
+			picture.ptrDst[i][j] = (picture.ptrDst[i][j] == 255 ? 1 : 0);
+			picture.ptrDstCopy[i][j] = (picture.ptrDstCopy[i][j] == 255 ? 1 : 0);
+		}
+	}
+
+	picture.cols = binaryMat.cols;
+	picture.rows = binaryMat.rows;
+
+	return picture;
+}
+
+extern "C"
+__declspec(dllexport)
+int Cols(char* path) {
+	auto picture = GetPicture(path);
+	
+	return picture.cols;
+}
+
+extern "C"
+__declspec(dllexport)
+int Rows(char* path) {
+	auto picture = GetPicture(path);
+
+	return picture.rows;
+}
+
+int* ToResult(std::vector<std::vector<double>> input, int rows, int cols)
+{
+	int* out = new int[cols * rows + 1];
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			out[i * cols + j] = input[i][j];
+		}
+	}
+
+	return out;
+}
+
+extern "C"
+__declspec(dllexport)
+int* Dilatation(int kernel, char* path)
+{
+	int WHITE = 1;
+	int BLACK = 0;
+
+	Picture picture = GetPicture(path);
+
+	omp_set_num_threads(3);
+#pragma omp parallel default (shared)
+#pragma omp for schedule(dynamic) collapse(6)
+	for (int i = 0; i < picture.rows; i++) {
+		for (int j = 0; j < picture.cols; j++) {
+
+			if (picture.ptrDst[i][j] == BLACK) {
+
+				for (int k = i - kernel / 2; k < i + kernel / 2; k++)
+				{
+					for (int l = j - kernel / 2; l < j + kernel / 2; l++)
+					{
+						if (k >= 0 && l >= 0 && k + kernel / 2 < picture.rows && l + kernel / 2 < picture.cols)
+							picture.ptrDstCopy[k][l] = BLACK;
+					}
+				}
+
+			}
+		}
+	}
+
+	return ToResult(picture.ptrDstCopy, picture.rows, picture.cols);
+}
+
+int* Erosion(int kernel, char* path)
+{
+	int WHITE = 1;
+	int BLACK = 0;
+
+	Picture picture = GetPicture(path);
+
+	omp_set_num_threads(3);
+#pragma omp parallel default (shared)
+#pragma omp for schedule(dynamic) collapse(6)
+	for (int i = 0; i < picture.rows; i++) {
+		for (int j = 0; j < picture.cols; j++) {
+
+			if (picture.ptrDst[i][j] == WHITE) {
+
+				for (int k = i - kernel / 2; k < i + kernel / 2; k++)
+				{
+					for (int l = j - kernel / 2; l < j + kernel / 2; l++)
+					{
+						if (k >= 0 && l >= 0 && k + kernel / 2 < picture.rows && l + kernel / 2 < picture.cols)
+							picture.ptrDstCopy[k][l] = WHITE;
+					}
+				}
+
+			}
+		}
+	}
+
+	return ToResult(picture.ptrDstCopy, picture.rows, picture.cols);
+}
+#endif //PCH_H
